b9b5467a89d195fcd1c126bdba8df78cf6bd4901
Fragalis <127601973+Fragalis@users.noreply.github.com>
2023-11-10 11:18:49 +0700

    updated input_reading module

--- STM32IDE/Buttons-Switches/Core/Inc/global.h
+++ STM32IDE/Buttons-Switches/Core/Inc/global.h
@@ -10,6 +10,14 @@
 
 #include "main.h"
 
-#define LED_CYCLE	500
+// TIMER
+#define TIMER_CYCLE						10
+#define LED_CYCLE						500
+
+// INPUT
+#define NO_OF_BUTTONS 					1
+#define DURATION_FOR_HOLD				1000 / TIMER_CYCLE
+#define BUTTON_IS_PRESSED				GPIO_PIN_RESET
+#define BUTTON_IS_RELEASED				GPIO_PIN_SET
 
 #endif /* INC_GLOBAL_H_ */
